11d2cf84f2f39778493ac487a2a9dc9f5d45f902
duy4nt <singhduyant@gmail.com>
2025-11-20 12:42:31 +0000

    completed the algo

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
+#include <vector>
 
-void insertion_sort() {}
+void insertion_sort(std::vector<int>& array, int size) {
+    for (int i = 1; i < size; i++) {
+        int key = array[i];
+        int j = i - 1;
 
-void print_array() {}
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+}
 
-void main() {
-    int array_size;
-    std::cout << "Enter the size of the array";
-    std::cin >> array_size;
+void print_array(std::vector<int>& array, int size) {
+    std::cout << "The elements of the array are: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << " ";
+    }
+}
 
-    int array[array_size];
-
-    for (int i= 0; i< array_size; i++) {
-        std::cout << "Enter the " << i+1 << " th element".
+void input_array(std::vector<int>& array, int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << "Enter the element " << i + 1 << " ";
         std::cin >> array[i];
     }
+}
 
-    std::cout << "The array elements are: ";
-    for (int i= 0; i< array_size; i++) {
-        cout << array[i] << " ";
-    }
+
+int main() {
+    int array_size;
+    std::cout << "Enter the size of the array: ";
+    std::cin >> array_size;
+
+    std::vector<int> array(array_size);
+
+    input_array(array, array_size);
+    print_array(array, array_size);
+    insertion_sort(array, array_size);
+    print_array(array, array_size);
 
     return 0;
-}   
+}

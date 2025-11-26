fab9f585a5cd9ed99adcf9da4caf794881f2b3a5
duy4nt <singhduyant@gmail.com>
2025-11-26 13:58:36 +0000

    wip

--- adj_mat_graph_rep.rs
+++ adj_mat_graph_rep.rs
@@ -8,14 +8,33 @@ fn main() {
     let nodes: usize = nodes.trim().parse::<usize>().expect("Unable to parse into int");
     println!("{:?}", nodes);
 
-    let mut matrix: Vec<Vec<i32>> = vec![vec![0; nodes]; nodes];
+    let mut matrix: Vec<Vec<u32>> = vec![vec![0; nodes]; nodes];
     println!("{:?}", matrix);
+
+    let mut vertex: Vec<char> = vec![' '; nodes];
 
 }
 
-fn add_edge(u: usize, v:usize, matrix: &mut Vec<Vec<i32>>) {
+fn add_edge(u: usize, v:usize, matrix: &mut Vec<Vec<u32>>) {
     if 0<=u && u<matrix.len() && 0<=v && v<matrix.len() {
         matrix[u][v]= 1;
         matrix[v][u]= 1;
+    }
+}
+
+fn add_vertex_data(vertex_data: &mut Vec<char>,vertex: u32, data: char ) {
+    if 0<=vertex && vertex<vertex_data.len() {
+        vertex_data[vertex] = data;
+    }
+}
+
+fn print_graph(vertex_data: &Vec<char>, matrix: &Vec<Vec<u32>>) {
+    println!("Adjacency Matrix");
+    for row in matrix {
+        println!(" {row}");
+    }
+    println!("\nVertex Data");
+    for vertex, data in vertex_data.iter().enumerate() {
+        println!("Vertex {}, {}", vertex, data);
     }
 }
